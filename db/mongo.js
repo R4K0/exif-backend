b4c88fcdda866800a4342a309cf97496ae4dd6cb
rako <milosz@rako.dev>
2021-01-06 22:46:10 +0000

    use env variable for mongo connection

--- db/mongo.js
+++ db/mongo.js
@@ -7,9 +7,9 @@ var connection;
 (async () => {
     try {
         //If we managed to connect, return the connection object to the variable
-        connection = await mongoose.connect(`mongodb://${config.host}:${config.port}/${config.collection}`, { useNewUrlParser: true, useUnifiedTopology: true })
+        connection = await mongoose.connect(process.env.mongo_connecturl ? `` : `mongodb://${process.env.PORT || config.host}:${config.port}/${config.collection}`, { useNewUrlParser: true, useUnifiedTopology: true })
 
-        console.log(`Connected to the database! port: ${config.port}`);
+        console.log(`Connected to the database!`);
     } catch (err) {
         // If we didn't log the error
         console.log(err);
